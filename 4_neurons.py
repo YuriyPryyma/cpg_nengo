9bce808d14608bae515efb026f934e317a8fa3fc
Yuriy Pryyma <priymayuriy@gmail.com>
2021-03-30 14:32:30 +0300

    Add fast changing step function

--- 4_neurons.py
+++ 4_neurons.py
@@ -82,40 +82,11 @@ def feedback(x):
     s1_new = (x[0]<=1 and x[0]>x[1]) or x[1]>1
     s2_new = (x[2]<=1 and x[2]>x[3]) or x[3]>1
     
-    # x[0] = x[0]*s1_new       + .01*((int(s1_old)-int(s1_new))<0);
-    # x[1] = x[1]*(not s1_new) + .01*((int(s1_old)-int(s1_new))>0);
-    # x[2] = x[2]*s2_new       + .01*((int(s2_old)-int(s2_new))<0);
-    # x[3] = x[3]*(not s2_new) + .01*((int(s2_old)-int(s2_new))>0);
-    
-    # print("\n\n")
-    # print("int(s1_old)", int(s1_old))
-    # print("int(s1_new)", int(s1_new))
-    # print("(int(s1_old)-int(s1_new))", (int(s1_old)-int(s1_new)))
-    # print("((int(s1_old)-int(s1_new))<0)", ((int(s1_old)-int(s1_new))<0))
-    # print(".01*((int(s1_old)-int(s1_new))<0)", .01*((int(s1_old)-int(s1_new))<0))
-    
     return x
 
 
-# mat = scipy.io.loadmat('x_0.mat')
-# x_0 = mat["X"].T
-# mat = scipy.io.loadmat('x_norm.mat')
-# x_norm = mat["X"].T
-# eval_points = np.vstack((x_0, x_norm))
-
-
-#[0.00391085 0.00166522 0.00389087 0.00168925]
-
 radius = np.sqrt(2)+0.1
-# eval_points = []
 
-# for i in range(10000):
-#     x = np.random.rand(4)*radius
-#     for _ in range(4):
-#         x = feedback(x)
-#         eval_points.append(x)
-
-# print(len(eval_points))
 
 model = nengo.Network(seed=40)
 with model:
@@ -125,27 +96,6 @@ with model:
     nengo.Connection(start, state)
     
     state_probe = nengo.Probe(state, synapse=tau)
-
-# with nengo.Simulator(model) as sim:
-#     sim.run(20)
-
-#     import matplotlib.pyplot as plt
-#     fig, axs = plt.subplots(4)
-#     axs[0].plot(sim.trange(), sim.data[state_probe][:, 0], label="x1")
-#     axs[1].plot(sim.trange(), sim.data[state_probe][:, 1], label="x2")
-#     axs[2].plot(sim.trange(), sim.data[state_probe][:, 2], label="x3")
-#     axs[3].plot(sim.trange(), sim.data[state_probe][:, 3], label="x4")
-#     fig.show()
-#     plt.show()
-
-# np.set_printoptions(precision=3)
-
-# for i in range(1000):
-#   c_state = sim.data[state_probe][i]
-#   print(i, c_state)
-
-
-# print(len(sim.data[state_probe]))
 
 
 
