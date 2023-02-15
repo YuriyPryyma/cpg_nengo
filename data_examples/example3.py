acde2509b9c02e98170643021c8154b70b9cc9c1
Yuriy Pryyma <priymayuriy@gmail.com>
2023-02-15 21:43:46 +0200

    Add documentation for example3,4

--- data_examples/example3.py
+++ data_examples/example3.py
@@ -1,3 +1,40 @@
+"""
+The third example builds on top of the integrator and threshold element.
+Now we want to have two antagonistic integrators, 
+where only one of them is active(integrating) at the moment 
+and the other is set to zero.
+We also want to switch "active" integrator 
+when one of them starts to saturate.
+
+For this, we would need two additional functions
+1) State element that stores current "active" integrator.
+2) Functionality to reset saturated Integator.
+
+Let's start with the state element and state change
+In our case State element is Ensemble with two neurons.
+As we do not need more to store binary variable (active state)
+
+Now the question is how we change the state 
+when one of the integrators saturates.
+We would use our threshold elements for this task. 
+We create two for each of the integrators. When the integrator saturates
+it triggers its corresponding threshold element. 
+The job of the threshold element is then to change the current state. 
+We do this with the simple connections that send in one case huge positive 
+and in another huge negative signal to 
+set the value of a state to 1 or -1 respectively.
+
+The next step is to reset the value of the integator.
+We do this using the inhibitory connection. It's possible in Nengo
+to access the decoding weights of an Ensemble. 
+Setting decoding weights to zero stops neurons from firing and
+resets the ensemble value to zero. We do this by sending strong negative signal to zero out the weights. 
+Our only job is to create two functions that inhibit integrators 
+when the state is 1 or the state is -1 and send nothing(zero) when opposite.
+
+The resulting system will oscillate and is a crucial building block for CPG
+
+"""
 import nengo
 
 model = nengo.Network()
